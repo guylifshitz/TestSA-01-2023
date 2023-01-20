7b0c8c16b223ceb751f452b33dfd8b6122f6537e
Guy <guylifshitz@gmail.com>
2023-01-20 11:47:35 +0100

    Setup a simple command line argument parse.

--- level1/piou.rb
+++ level1/piou.rb
@@ -5,3 +5,16 @@
 # Il sera exécuté de la façon suivante :
 # ruby piou.rb encode '...'
 # ruby piou.rb decode '...'
+
+# TODO change this to use options parser
+translation_direction = ARGV[0]
+sentence = ARGV[1]
+
+case translation_direction
+when "encode"
+  puts "encode"
+when "decode"
+  puts "decode"
+else
+  puts "Non valid first parameter."
+end
