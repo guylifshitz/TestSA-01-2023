570778c49187a76881d596ab872901dac0a02608
Guy <guylifshitz@gmail.com>
2023-01-20 12:24:58 +0100

    Add first RSpec (for Test driven development)

--- level1/piou_test.rb
+++ level1/piou_test.rb
@@ -1,4 +1,32 @@
 # Ce fichier sera lancé pour tester l'application
 # Veillez à tester les situations et erreurs inattendues
 
-require_relative 'piou'
+require_relative "piou"
+
+describe Translator do
+  describe "translating pioupiou to french" do
+    context "with valid input" do
+      it "translates correctly" do
+        fail :not_implemented
+      end
+    end
+    context "with bad input" do
+      it "should handle bad input" do
+        fail :not_implemented
+      end
+    end
+  end
+
+  describe "translating french to pioupiou" do
+    context "with valid input" do
+      it "translates correctly" do
+        fail :not_implemented
+      end
+    end
+    context "with bad input" do
+      it "should handle bad input" do
+        fail :not_implemented
+      end
+    end
+  end
+end
